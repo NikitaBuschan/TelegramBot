e844031bc5aa56fc12c283fd026129f5f1897c52
kaps.nikita <kaps.nikitabushhan@gmail.com>
2020-03-31 21:40:27 +0300

    Add project files.

---
+++ TelegramBot/Models/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TelegramBot.Models
+{
+    public class AppSettings
+    {
+        public static string Url { get; set; } = "https://URL:443/{0}";
+        public static string Name { get; set; } = "test";
+        public static string Key { get; set; } = "964302173:AAHOdv2ukNh66AoRi8_btGdbP2I5Dnjqa-U";
+    }
+}
